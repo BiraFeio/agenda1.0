657a9985cdb1a3cf4ee258bf480ea86e16672673
BiraFeio <bira_feio@hotmail.com>
2022-12-13 11:56:03 -0400

    DEBUG false, allowed_hosts 127.0.0.1, versão de produção 1.2

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-59r5t9-s&oak!3_#_bck$)=mz7@azlbg_rh6&+dxqj+de7(@6o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','agenda.com']
 
 
 # Application definition
